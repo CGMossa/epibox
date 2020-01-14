3ad50558c16443f89ab3f141ba24138c4a53f253
cgmossa <cgmossa@gmail.com>
2020-01-15 00:00:52 +0100

    Lazy evaluation makes this wrong
    Registrering expressions as they go does not work due to lazy-evaluation.

--- src/population.rs
+++ src/population.rs
@@ -1,7 +1,13 @@
 use std::collections::HashMap;
+use std::fmt::{Display, Error, Formatter};
 
-type Count = u64;
+type Count = f64;
 type Rate = f64;
+
+///
+/// This turned out to be a failed experiment. The expressions in the equation changes value, when
+/// we are to calculate them, due to them being coupled.
+///
 
 #[derive(Default)]
 struct Population {
@@ -25,7 +31,7 @@ struct DiseaseTransition {
     //    dynamic: fn(Count, Count) -> Rate,
 }
 
-#[derive(Hash, PartialEq, Eq, Debug)]
+#[derive(Hash, PartialEq, Eq, Debug, Copy, Clone)]
 enum DiseaseCompartment {
     Susceptible,
     Exposed, //also Latent
@@ -54,11 +60,28 @@ impl Population {
     }
 
     fn update_disease_states(&mut self) {
+        let mut next_counts = self.count.clone();
         for (transition, boxed_transition) in self.transitions.iter() {
-            let from_count = self.count.get(&transition.from);
-            let to_count = self.count.get(&transition.to);
-            let dynamic = boxed_transition.call((from_count, to_count));
+            let from_count = *self.count.get(&transition.from).unwrap();
+            let to_count = *self.count.get(&transition.to).unwrap();
+            let from_diff = boxed_transition(from_count, to_count);
+            //            let to_diff = -from_diff;
+
+            //            dbg!((&transition.from, &transition.to));
+            //            dbg!((from_count, to_count), (from_diff, -from_diff));
+
+            *next_counts.entry(transition.from).or_default() = from_count + from_diff;
+            *next_counts.entry(transition.to).or_default() = to_count - from_diff;
         }
+        self.count = next_counts;
+    }
+}
+
+impl Display for Population {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{:.6?}", self.count)?;
+
+        Ok(())
     }
 }
 
@@ -70,21 +93,24 @@ fn building_a_sir_population() {
     let recovery_rate = 0.5;
 
     let sir_population = Population::new()
-        .add_compartment(Susceptible, 0)
-        .add_compartment(Infected, 0)
-        .add_compartment(Recovered, 0);
-    let sir_population = sir_population
+        .add_compartment(Susceptible, 50.)
+        .add_compartment(Infected, 1.)
+        .add_compartment(Recovered, 0.);
+    let mut sir_population = sir_population
         .add_transition(
             Susceptible,
             Infected,
-            Box::new(move |sus, inf| -((sus * inf) as Rate) * infection_rate),
+            Box::new(move |sus, inf| -sus * inf * infection_rate),
         )
         .add_transition(
             Infected,
             Recovered,
-            Box::new(move |inf, recover| -recovery_rate * (inf as f64)),
+            Box::new(move |inf, _recover| -recovery_rate * inf),
         );
 
-    sir_population.update_disease_states();
-    //    println!("{:#?}", sir_population);
+    println!("{}", sir_population);
+    for time in 0..5 {
+        sir_population.update_disease_states();
+        println!("time {} => {:.6}", time, sir_population);
+    }
 }
