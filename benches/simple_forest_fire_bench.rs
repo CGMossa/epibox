dc7eb595a8ddf0b3ee0df2b5032d181b96af7f7f
cgmossa <cgmossa@gmail.com>
2020-01-24 14:26:44 +0100

    adjusted benchmark
    Takes to loong

--- benches/simple_forest_fire_bench.rs
+++ benches/simple_forest_fire_bench.rs
@@ -6,8 +6,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("percolation thr. 50 x 1", |b| {
         b.iter(|| percolation_threshold(black_box(50), black_box(0.5), black_box(1)))
     });
-    c.bench_function("percolation thr. 50 x 200", |b| {
-        b.iter(|| percolation_threshold(black_box(50), black_box(0.5), black_box(200)))
+    c.bench_function("percolation thr. 50 x 10", |b| {
+        b.iter(|| percolation_threshold(black_box(50), black_box(0.5), black_box(10)))
     });
 }
 
